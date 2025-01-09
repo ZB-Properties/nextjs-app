10cb6627c1784b134d823e90c7df6ac32a1961f3
Shope Alli-B <zoeyalli921@gmail.com>
2025-01-09 07:29:08 -0700

    first commit

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -2,27 +2,29 @@ import {getProfileByUsername, getUserLikedPosts, getUserPosts, isFollowing, } fr
   import { notFound } from "next/navigation";
   import ProfilePageClient from "./ProfilePageClient";
   
-  export async function generateMetadata({ params }: { params: { username: string } }) {
+  export async function generateMetadata(props: { params: Promise<{ username: string }> }) {
+    const params = await props.params;
     const user = await getProfileByUsername(params.username);
     if (!user) return;
-  
+
     return {
       title: `${user.name ?? user.username}`,
       description: user.bio || `Check out ${user.username}'s profile.`,
     };
   }
   
-  async function ProfilePageServer({ params }: { params: { username: string } }) {
+  async function ProfilePageServer(props: { params: Promise<{ username: string }> }) {
+    const params = await props.params;
     const user = await getProfileByUsername(params.username);
-  
+
     if (!user) notFound();
-  
+
     const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
       getUserPosts(user.id),
       getUserLikedPosts(user.id),
       isFollowing(user.id),
     ]);
-  
+
     return (
       <ProfilePageClient
         user={user}

238fae84f0d9786960be0e82a00b87282fb1931a
Shope Alli-B <zoeyalli921@gmail.com>
2025-01-09 07:35:39 -0700

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
    
- 
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
 };
 
 export default nextConfig;
